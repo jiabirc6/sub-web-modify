6f274976783295415cc3d380f52baa8392de37e7
jiabirc6 <153730546+jiabirc6@users.noreply.github.com>
2026-04-22 15:51:55 +0800

    Update Subconverter.vue

--- src/views/Subconverter.vue
+++ src/views/Subconverter.vue
@@ -348,10 +348,14 @@ export default {
           "dlj.tf": "https://dlj.tf/short",
         },
         customBackend: {
-          "CM提供-负载均衡后端": "https://subapi.cmliussss.net",
-          "CM提供-应急备用后端": "https://subapi.fxxk.dedyn.io",
-          "肥羊提供-增强型后端": "https://url.v1.mk",
-          "肥羊提供-备用后端": "https://api.v1.mk",
+          "自用专属后端": "https://sub.190009.xyz/sub",
+      "CM负载均衡后端【vless reality+hy1+hy2】": "https://subapi.cmliussss.net",
+      "CM应急备用后端【vless reality+hy1+hy2】": "https://subapi.fxxk.dedyn.io",
+      "肥羊增强型后端【vless reality+hy1+hy2】": "https://url.v1.mk",
+      "肥羊备用后端【vless reality+hy1+hy2】": "https://sub.d1.mk",
+      "nameless13提供": "https://www.nameless13.com",
+      "subconverter作者提供": "https://sub.xeton.dev",
+      "sub-web作者提供": "https://api.wcc.best",
         },
         backendOptions: [
           { value: "https://subapi.cmliussss.net" },
